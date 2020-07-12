13f48ac58c6f3572c38d1f2c3684e42b7d8f083d
Yin Ruchao <1433538263@qq.com>
2020-07-12 09:02:09 +0800

    first commit

---
+++ Travel/src/pages/home/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <home-header></home-header>
+    <home-swiper :swiperList="swiperList"></home-swiper>
+    <home-icons :iconList="iconList"></home-icons>
+    <home-recommend :hotList="hotList"></home-recommend>
+    <home-weekend :weekendList="weekendList"></home-weekend>
+    </div>
+</template>
+
+<script>
+// import ApiUrl from '@/config/api_url'
+import axios from 'axios'
+import ApiUrl from '@/config/api_url'
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Home',
+  components: {HomeHeader, HomeSwiper, HomeIcons, HomeRecommend, HomeWeekend},
+  data () {
+    return {
+      swiperList: [],
+      iconList: [],
+      hotList: [],
+      weekendList: []
+    }
+  },
+  computed: mapState([
+    'city'
+  ]),
+  activated () {
+    // 当页面加载时，进行判断，城市发生了改变
+    if (this.LastCity !== this.city) {
+      this.getHomeInfo()
+      this.LastCity = this.city
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get(ApiUrl.api + 'index.json')
+        .then(this.handleIndex)
+    },
+    handleIndex (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        this.swiperList = res.data.swiperList
+        this.iconList = res.data.iconList
+        this.hotList = res.data.hotList
+        this.weekendList = res.data.weekendList
+      }
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
+    this.LastCity = this.city
+  }
+}
+</script>
+
+<style>
+
+</style>
